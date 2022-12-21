19b28aac1b2f7b0c1b62cec0e6a1ed39251f029e
Joel Perron <103043785+Cyborg1975@users.noreply.github.com>
2022-12-21 07:52:07 -0500

    Completed module

---
+++ budget/FrequentExpenses.py
@@ -0,0 +1,16 @@
+from . import Expense
+import collections
+
+expenses = Expense.Expenses()
+
+expenses.read_expenses(data/spending_data.csv)
+
+spending_categories = []
+
+for expense in expenses.list():
+    expense.category.append(spending_categories)
+
+
+spending_counter = collections.Counter(spending_categories)
+
+print(spending_counter)
