c0fd0a8c08d516118d92f5cf6ef5de181d03daf6
Joel Perron <103043785+Cyborg1975@users.noreply.github.com>
2023-01-02 23:19:48 -0500

    Completed module

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -49,7 +49,8 @@ def main():
 
     fig, ax = plt.subplots()
     labels = ['Expenses', 'Overages', 'Budget']
-
+    values = [myBudgetList.sum_expenses, myBudgetList.sum_overages, myBudgetList.budget]
+    ax.bar(labels, values, color=['green','red','blue'])
 
 if __name__ == "__main__":
     main()
