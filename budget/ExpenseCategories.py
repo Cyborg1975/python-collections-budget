402c4380784722296ebb73a4723327353e7e8b95
Joel Perron <103043785+Cyborg1975@users.noreply.github.com>
2023-01-02 23:53:46 -0500

    Completed module

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -6,6 +6,8 @@ def main():
     expenses.read_expenses('data/spending_data.csv')
     divided_for_loop = expenses.categorize_for_loop()
 
+    divided_set_comp = expenses.categorize_set_comprehension()
+
 
 if __name__ == "__main__":
     main()
