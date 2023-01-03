c405136eb7e4e8e98d4ae389e0afa4057fd10858
Joel Perron <103043785+Cyborg1975@users.noreply.github.com>
2023-01-03 00:08:18 -0500

    Completed module

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -17,7 +17,25 @@ def main():
             b.issubset(a)):
             print("Sets are NOT equal by subset test")
 
-    timeit.timeit(stmt = "pass" , setup = , number=100000 , globals=globals())
+    print(timeit.timeit(stmt = "expenses.categorize_for_loop()",
+                        setup=
+                        '''
+from . import Expense
+expenses = Expense.Expenses()
+expenses.read_expenses('data/spending_data.csv')
+                        ''',
+                        number=100000,
+                        globals=globals()))
+
+    print(timeit.timeit(stmt = "expenses.categorize_set_comprehension()",
+                        setup=
+                        '''
+from . import Expense
+expenses = Expense.Expenses()
+expenses.read_expenses('data/spending_data.csv')
+                        ''',
+                        number=100000,
+                        globals=globals()))
 
 if __name__ == "__main__":
     main()
